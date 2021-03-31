aeba6e309b2b157694ae3f12b89547332866912f
gsk <dfyz0r@list.ru>
2021-03-31 13:24:42 +0300

    server initial commit

---
+++ server/src/index.ts
@@ -0,0 +1,28 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config.js';
+
+import cors from './middlewares/cors.js';
+
+const PORT = process.env.PORT;
+
+const app = express();
+app.use(cors);
+app.use(express.json());
+
+(async () => {
+  try {
+    if (!process.env.DATABASE_URL) {
+      return console.error('Добавь ссылку для подключения к MongoDB Cluster: https://cloud.mongodb.com/v2/6064444ce399f0713425f88e#clusters/connect?clusterId=Cluster0');
+    }
+
+    await mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true });
+
+    app.listen(PORT, () => {
+      console.log(`\nСервер запущен на порту ${PORT}`);
+    });
+
+  } catch (err) {
+    console.error(err);
+  }
+})();

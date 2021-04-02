afb307e1a5c065f376ca4cb7c34a61ba2f988e13
gsk <dfyz0r@list.ru>
2021-04-02 18:22:51 +0300

    feat(client): add constants

---
+++ client/src/const.ts
@@ -0,0 +1,13 @@
+export const BACKEND_URL = 'http://localhost:8432/';
+export const API_URL = `${BACKEND_URL}api/`;
+
+export const AppRoute = {
+  ROOT: '/',
+  LOGIN: '/login',
+  REGISTRATION: '/registration',
+} as const;
+
+export const APIRoute = {
+  LOGIN: 'auth/login',
+  REGISTRATION: 'auth/registration',
+}

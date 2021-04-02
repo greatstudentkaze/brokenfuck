a008f1d5404a36bd912be0be916664a071dacfd7
gsk <dfyz0r@list.ru>
2021-04-02 18:24:49 +0300

    feat(client/api): add AuthAPI (DAL)

---
+++ client/src/api/auth.ts
@@ -0,0 +1,41 @@
+import axios from 'axios';
+
+import { API_URL, APIRoute } from '../const';
+
+import { IUser, IUserData } from '../interfaces';
+
+type TokenAndUserData = {
+  user: IUserData,
+  token: string,
+};
+
+type Message = {
+  message: string,
+};
+
+export namespace ResponseType {
+  export type Login = TokenAndUserData;
+
+  export type Register = Message;
+}
+
+class AuthAPI {
+  async login (email: IUser['email'], password: IUser['password']): Promise<TokenAndUserData> {
+    try {
+      const response = await axios.post<ResponseType.Login>(`${API_URL}${APIRoute.LOGIN}`, { email, password });
+      return response.data;
+    } catch (err) {
+      throw new Error(err.response.data.message);
+    }
+  }
+  async register (email: IUser['email'], password: IUser['password']): Promise<Message> {
+    try {
+      const response = await axios.post<ResponseType.Register>(`${API_URL}${APIRoute.REGISTRATION}`, { email, password });
+      return response.data;
+    } catch (err) {
+      throw new Error(err.response.data.message);
+    }
+  }
+}
+
+export default new AuthAPI();

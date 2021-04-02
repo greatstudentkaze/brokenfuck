e79d11ef553767ef7d574a97f1f7f3888769b274
gsk <dfyz0r@list.ru>
2021-04-02 18:27:33 +0300

    feat(client/redux): add middleware for redirecting

---
+++ client/src/redux/middlewares/redirect.ts
@@ -0,0 +1,15 @@
+import { Middleware, MiddlewareAPI } from 'redux';
+import browserHistory from '../../browser-history';
+
+import { AppDispatch } from '../store';
+import { REDIRECT_TO_ROUTE } from '../actions/types/app';
+
+const redirect: Middleware = (_store: MiddlewareAPI) => (next: AppDispatch) => (action) => {
+  if (action.type === REDIRECT_TO_ROUTE) {
+    browserHistory.push(action.payload);
+  }
+
+  return next(action);
+};
+
+export default redirect;
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,10 +1,12 @@
 import { applyMiddleware, createStore } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
+import thunk from 'redux-thunk'
+
+import redirect from './middlewares/redirect';
 
 import rootReducer from './reducer';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk, redirect)));
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;

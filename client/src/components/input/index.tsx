38aaf1c6a6bf64be9ba04b4140c3eb43ae131ca1
gsk <dfyz0r@list.ru>
2021-04-02 18:34:00 +0300

    feat(client/components): add controlled Input component

---
+++ client/src/components/input/index.tsx
@@ -0,0 +1,23 @@
+import React, { ChangeEvent, Dispatch, SetStateAction } from 'react';
+
+import './css/input.css';
+
+type Props = {
+  type: 'text' | 'email' | 'password',
+  placeholder: string,
+  value: string,
+  setValue: Dispatch<SetStateAction<string>>,
+  autoComplete?: string,
+  className?: string,
+  required: boolean
+};
+
+const Input = ({ type, placeholder, value, setValue, ...rest }: Props) => {
+  const handleChange = (evt: ChangeEvent<HTMLInputElement>) => setValue(evt.target.value);
+
+  return (
+    <input {...rest} className={`input ${rest.className ?? ''}`} type={type} placeholder={placeholder} value={value} onChange={handleChange} />
+  );
+};
+
+export default Input;
